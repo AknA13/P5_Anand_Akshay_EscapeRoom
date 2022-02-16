071d3fbfac6bd5f0c90811a82eeb7d217e3ebcc6
AknA13 <95551900+AknA13@users.noreply.github.com>
2022-02-16 12:40:24 -0800

    Add files via upload

---
+++ P5_Anand_Akshay_EscapeRoom/src/WandOfKeySummoning.java
@@ -0,0 +1,15 @@
+
+public class WandOfKeySummoning extends Item{
+
+	public WandOfKeySummoning(String name, String description) {
+		super(name, description);
+		
+	}
+
+	@Override
+	public void use() {
+		System.out.println("Gold sparkles burst from the wand! A gold key appears. This appears to be the key to the door.");
+		getRoom().add(new UselessItem("gold_key",""));
+	}
+
+}
